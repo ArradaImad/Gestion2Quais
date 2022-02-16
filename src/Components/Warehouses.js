6f8eb41d9cae8a191db87d21f30b60de4b82dff1
Imad Arrada <imad-seddik.arrada@capgemini.com>
2022-02-16 14:10:47 +0100

    Creation of a mock component that retrieves fake data from backend

--- src/Components/Warehouses.js
+++ src/Components/Warehouses.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react';
+
 function Warehouses() {
+    const [warehouses, setWarehouses] = useState([]);
     // Récupération des données de l'API
+    useEffect(() => {
+        let options = {
+            mode: 'no-cors',
+            method: "GET",
+            headers: {
+                 "Content-Type": "application/json",
+                 'Access-Control-Allow-Origin': '*',
+                 'Access-Control-Allow-Headers': 'Content-Type',
+                 'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+                 'Authorization': 'Bearer key',
+            }
+        }
+        fetch("http://localhost:3001/warehouses")
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                //console.log(data);
+                setWarehouses(data);
+            }
+        );
+
+    }, []);
+    
     return (
         <>
-        
+        {warehouses.map(w => <p key={w.id}>{w.email}</p>)}
         </>
     );
 }
